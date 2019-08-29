6acf449c5b3631b9bafe5d757f06a00ce4d042aa
mcase3 <54606922+mcase3@users.noreply.github.com>
2019-08-29 18:47:20 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Markus Case Jon Haag
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Mark and Jon are the best hackers in the world!\n");
+
+  return 0;
+}
